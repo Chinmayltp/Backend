e3ee777dbb8439df693171f0714009968bc3d441
Chinmayltp <jainchinmay916@gmail.com>
2024-02-09 12:52:47 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const port = 5000;
+const cors = require("cors");
+const mongoDB = require("./db");
+mongoDB();
+
+//app.use(cors());
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    headers: ["Content-Type"],
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  })
+);
+
+app.use(function (req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  return res.send("Hello World!");
+});
+
+app.use("/api", require("./Routes/CreateUser"));
+
+app.listen(port, () => {
+  console.log(`Example app listening on http://localhost:${port}`);
+});
